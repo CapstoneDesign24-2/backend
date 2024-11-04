b8a33beb33bf3eff30cf84acce1959e27d0b7afc
SHIMHUN <shimhun88@naver.com>
2024-11-04 19:34:18 +0900

    chore: BaseEntity 상속

---
+++ src/main/java/errorcode/backend/domain/entity/User.java
@@ -0,0 +1,21 @@
+package errorcode.backend.domain.entity;
+
+import errorcode.backend.domain.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user") // 실제 몽고 DB 컬렉션 이름
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    private String id;
+    private String name;
+    private String password;
+    private String email;
+
+}
