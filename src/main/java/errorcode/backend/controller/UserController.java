8858506d8e0a221ad8cbbfb3c09fe3b0b977135d
SHIMHUN <shimhun88@naver.com>
2024-11-04 18:09:57 +0900

    Feat: User Controller 추가

---
+++ src/main/java/errorcode/backend/controller/UserController.java
@@ -0,0 +1,19 @@
+package errorcode.backend.controller;
+
+import errorcode.backend.entity.User;
+import errorcode.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public User signUp(@RequestBody User user){
+        return userService.signUp(user);
+    }
+}
