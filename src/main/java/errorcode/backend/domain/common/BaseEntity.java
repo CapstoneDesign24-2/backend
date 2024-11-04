7f2c0caec62979def8cb99108527129e98ab2987
SHIMHUN <shimhun88@naver.com>
2024-11-04 19:33:43 +0900

    Feat: BaseEntity 추가

--- src/main/java/errorcode/backend/domain/common/BaseEntity.java
+++ src/main/java/errorcode/backend/domain/common/BaseEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 
 @Getter
-public abstract class BaseEntity {
+public abstract class BaseEntity{
     @CreatedDate
     private LocalDateTime createdAt;
 
