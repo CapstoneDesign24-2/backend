3ff903329a83c59c0e29bf7061926cb7108830bd
SHIMHUN <shimhun88@naver.com>
2024-11-04 18:10:06 +0900

    Feat: User Repository 추가

---
+++ src/main/java/errorcode/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package errorcode.backend.repository;
+
+import errorcode.backend.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
