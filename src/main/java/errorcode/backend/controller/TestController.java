14cf5fbf7b4fc59a7fc3b630bb0d28e2671aa8ea
SHIMHUN <shimhun88@naver.com>
2024-11-11 00:50:28 +0900

    chore: log 기능 추가

--- src/main/java/errorcode/backend/controller/TestController.java
+++ src/main/java/errorcode/backend/controller/TestController.java
@@ -1,13 +1,15 @@
 package errorcode.backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class TestController {
 
     @GetMapping("/")
     public String test(){
+        log.info("test");
         return "Hello World";
     }
 }
