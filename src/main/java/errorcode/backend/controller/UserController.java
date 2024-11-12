936c075e1b5010c4d5897adf3e1e93b20eb98d7c
SHIMHUN <shimhun88@naver.com>
2024-11-12 21:44:32 +0900

    feat: 유저 중복 관련 controller 추가

--- src/main/java/errorcode/backend/controller/UserController.java
+++ src/main/java/errorcode/backend/controller/UserController.java
@@ -1,5 +1,8 @@
 package errorcode.backend.controller;
 
+import errorcode.backend.apipayload.ApiResponse;
+import errorcode.backend.apipayload.code.status.ErrorStatus;
+import errorcode.backend.apipayload.code.status.SuccessStatus;
 import errorcode.backend.domain.entity.User;
 import errorcode.backend.dto.UserLogin;
 import errorcode.backend.service.UserService;
@@ -20,4 +23,20 @@ public class UserController {
 
     @PostMapping("/login")
     public User login(@RequestBody UserLogin userLogin){return userService.login(userLogin);}
+
+    @GetMapping("/duplicate/id")
+    public ApiResponse duplicateId(@RequestParam("userId") String userId){
+        if(userService.duplicateId(userId)){
+            return ApiResponse.onFailure(ErrorStatus.USER_ID_DUPLICATE_ERROR.getCode(), ErrorStatus.USER_ID_DUPLICATE_ERROR.getMessage(), "fail");
+        }
+        return ApiResponse.onSuccess(SuccessStatus._OK) ;
+    }
+
+    @GetMapping("/duplicate/nickname")
+    public ApiResponse duplicateNickname(@RequestParam("nickname") String nickname){
+        if(userService.duplicateNickname(nickname)){
+            return ApiResponse.onFailure(ErrorStatus.USER_NICKNAME_DUPLICATE_ERROR.getCode(), ErrorStatus.USER_NICKNAME_DUPLICATE_ERROR.getMessage(), "fail");
+        }
+        return ApiResponse.onSuccess(SuccessStatus._OK) ;
+    }
 }
