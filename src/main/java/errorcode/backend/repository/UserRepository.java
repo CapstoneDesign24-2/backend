476a21d66322bbd089ff77a3ddec37b216c985e5
SHIMHUN <shimhun88@naver.com>
2024-11-12 21:44:23 +0900

    feat: 유저 중복 관련 repository 추가

--- src/main/java/errorcode/backend/repository/UserRepository.java
+++ src/main/java/errorcode/backend/repository/UserRepository.java
@@ -4,7 +4,10 @@ import errorcode.backend.domain.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByLoginIdAndPassword(String login_id, String password);
+    Optional<User> findByNickname(String nickname);
 }
