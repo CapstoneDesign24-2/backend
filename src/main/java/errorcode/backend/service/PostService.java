3345535abec6325ac33fc112ae3ef60c867ebb2b
SHIMHUN <shimhun88@naver.com>
2024-11-12 09:30:48 +0900

    feat: 게시글 생성, 조회 service

--- src/main/java/errorcode/backend/service/PostService.java
+++ src/main/java/errorcode/backend/service/PostService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -16,9 +18,13 @@ public class PostService {
     private UserRepository userRepository;
 
     public Post save(Post post) {
-        if(userRepository.existsById(post.getUser_id())){
-            return postRepository.save(post);
-        }
-        return null;
+//        if(!userRepository.existsById(post.getUser_id())){
+//            return null;
+//        }
+        return postRepository.save(post);
+    }
+
+    public Post findById(String post_id) {
+        return postRepository.findById(post_id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다"));
     }
 }
