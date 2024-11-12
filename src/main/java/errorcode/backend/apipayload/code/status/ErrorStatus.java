f5dabee5cb71598dbcd16e259939bf1cf7b31781
SHIMHUN <shimhun88@naver.com>
2024-11-12 21:45:43 +0900

    chore: 유저 중복 관련 ErrorStatus 응답 추가

--- src/main/java/errorcode/backend/apipayload/code/status/ErrorStatus.java
+++ src/main/java/errorcode/backend/apipayload/code/status/ErrorStatus.java
@@ -15,6 +15,11 @@ public enum ErrorStatus implements BaseErrorCode {
     _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+
+
+    USER_ID_DUPLICATE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "USER3001","중복된 유저 아이디가 존재합니다"),
+    USER_NICKNAME_DUPLICATE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "USER3002","중복된 유저 닉네임이 존재합니다"),
+
     ;
 
     private final HttpStatus httpStatus;
